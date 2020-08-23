1b8fd4f0685d0d605bcb3e01c719a6ecc853d871
APXD-git <arkamtg.pramanik@gmail.com>
2020-08-23 18:41:10 +0530

    fix pm logger

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -62,8 +62,7 @@ if ENV:
         MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 3))
         # set to True if you want to log PMs to your PM_LOGGR_BOT_API_ID
         NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", False))
-        # send .get_id in any channel to forward all your NEW PMs to this group
-        PM_LOG_GRP_ID=
+        # send .get_id in any channel to forward all your NEW PMs to this group       
         PM_LOG_GRP_ID = os.environ.get("PM_LOG_GRP_ID", None)
         if PM_LOG_GRP_ID:
             PM_LOG_GRP_ID = int(PM_LOG_GRP_ID)
