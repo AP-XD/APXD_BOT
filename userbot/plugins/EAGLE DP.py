0f3b637dfa5a531a072a3dc310ab5690fa1eea36
SP-PIKACHU <somnatypaul007@gmail.com>
2020-09-14 21:09:16 +0530

    added untracked files

---
+++ userbot/plugins/EAGLE DP.py
@@ -0,0 +1,21 @@
+import random, re
+from uniborg.util import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="dp ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("😎Jaha matter bada hota hai😎")
+        await asyncio.sleep(2)
+        await event.edit("Eagle DP 😈wahi khada hota hai 💪")
+        await asyncio.sleep(2)
+        await event.edit("😡Tera baap hu madharchod😒")
+        await asyncio.sleep(2)
+        await event.edit("😏Naam yad rakhna😏")
+        await asyncio.sleep(2)
+        await event.edit("😎Tera baap EAGLE DP🤓")
+        await asyncio.sleep(2)
+        await event.edit("🔥Eagle DP🔥")
+        await asyncio.sleep(2)
+        await event.edit("😎Jaha matter bada hota hai😎. Eagle DP wahi khada hota hai😏. Tera baap hu madharchod😈. Naam yad rakhna😡. Tera baap EAGLE DP. 🔥🔥🔥🔥🔥")
---
+++ userbot/plugins/songs.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+from userbot.utils import admin_cmd
+import glob
+
+@borg.on(admin_cmd("song ?(.*)"))
+async def _(event):
+    song = url = event.pattern_match.group(1) + " " + "song"
+    if not song:
+        await event.edit("`Enter song name`")
+        return
+    await event.edit("Processing...")
+    os.system(f"youtube-dl -x --audio-format mp3 --add-metadata --embed-thumbnai 'ytsearch:{song}'")
+    l = glob.glob("*.mp3")
+    if not l:
+        await event.edit("`Song not found`")
+        return
+    await event.client.send_file(event.chat_id, l, supports_streaming=True, reply_to=event.message)
+    await event.delete()
+    subprocess.check_output("rm -rf *.mp3",shell=True)
