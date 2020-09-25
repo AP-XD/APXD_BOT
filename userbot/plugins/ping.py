9724651f596358f9cb6a5dcbb2da6ccf9aee7a32
APXD-git <arkamtg.pramanik@gmail.com>
2020-09-25 21:12:42 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -1,14 +1,48 @@
-from telethon import events
-from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from datetime import datetime
+# special thanks to Sur_vivor 
+# Re-written for TeleBot by @its_xditya
 
+from telethon import events
+from datetime import datetime
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot.__init__ import StartTime
+import time
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+#@command(pattern="^.ping$")
 @borg.on(admin_cmd(pattern="ping$"))
 @borg.on(sudo_cmd(pattern="ping$", allow_sudo=True))
 async def _(event):
-    starkislub = await edit_or_reply(event, "`Pong !`")
     if event.fwd_from:
         return
     start = datetime.now()
+    await event.edit("🏓 Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
-    await starkislub.edit("**Pong!**\n`{}`\n Bot of PERU Master ༒★彡☣️ 🇦 🇵 🇽 🇩 ☣️彡★༒ ".format(ms))
+    uptime = get_readable_time((time.time() - StartTime))
+    await event.edit(f"🏓Ping speed: {ms}\n🤖Uptime: {uptime} \n Bot of PERU Master ༒★彡☣️ 🇦 🇵 🇽 🇩 ☣️彡★༒ ")
