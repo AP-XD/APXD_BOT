4b849db58436641720c9d2d935e28296c332e1a7
APXD-git <63340491+APXD-git@users.noreply.github.com>
2020-10-14 14:48:41 +0530

    del concurrent

--- userbot/plugins/shhot.py
+++
@@ -1,8 +0,0 @@
-from userbot.utils import admin_cmd 
-
-@borg.on(admin_cmd(pattern = "shoot$"))
-async def _(cat):
-    await cat.edit("_/﹋\_\
-                           \n(҂`_´)\
-                           \n<,︻╦╤─ ҉ - -\
-                           \n_/﹋\_")
