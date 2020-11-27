e208fe32f7681cc324fe901d2ddf816609ad1e4f
APXD-git <63340491+APXD-git@users.noreply.github.com>
2020-11-27 11:26:48 +0530

    Update _inlinebot.py

--- fridaybot/modules/_inlinebot.py
+++ fridaybot/modules/_inlinebot.py
@@ -107,41 +107,6 @@ async def on_plug_in_callback_query_handler(event):
         reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-
-@tgbot.on(
-    events.callbackquery.CallbackQuery(  # pylint:disable=E0602
-        data=re.compile(b"us_plugin_(.*)")
-    )
-)
-async def on_plug_in_callback_query_handler(event):
-    if not event.query.user_id == bot.uid:
-        sedok = "Who The Fuck Are You? Get Your Own Friday."
-        await event.answer(sedok, cache_time=0, alert=True)
-        return
-    plugin_name = event.data_match.group(1).decode("UTF-8")
-    if plugin_name in CMD_HELP:
-        help_string = f"**💡 PLUGIN NAME 💡 :** `{plugin_name}` \n{CMD_HELP[plugin_name]}"
-    reply_pop_up_alert = help_string
-    reply_pop_up_alert += "\n\n**(C) @FRIDAYOT** ".format(plugin_name)
-    if len(reply_pop_up_alert) >= 4096:
-        crackexy = "`Pasting Your Help Menu.`"
-        await event.answer(crackexy, cache_time=0, alert=True)
-        out_file = reply_pop_up_alert
-        url = "https://del.dog/documents"
-        r = requests.post(url, data=out_file.encode("UTF-8")).json()
-        url = f"https://del.dog/{r['key']}"
-        await event.edit(
-            f"Pasted {plugin_name} to {url}",
-            link_preview=False,
-            buttons=[[custom.Button.inline("Go Back", data="backme")]],
-        )
-    else:
-        await event.edit(
-            message=reply_pop_up_alert,
-            buttons=[[custom.Button.inline("Go Back", data="backme")]],
-        )
-
-
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"terminator")))
 async def rip(event):
     if event.query.user_id == bot.uid:
@@ -233,6 +198,7 @@ async def on_plug_in_callback_query_handler(event):
         help_string = f"**💡 PLUGIN NAME 💡 :** `{plugin_name}` \n{CMD_HELP[plugin_name]}"
     reply_pop_up_alert = help_string
     reply_pop_up_alert += "\n\n**(C) @FRIDAYOT** ".format(plugin_name)
+    fci = [custom.Button.inline("◤✞ 𝕸𝖆𝖎𝖓 𝕸𝖊𝖓𝖚 ✞◥", data="backme"),custom.Button.inline("◤✞ 𝕮𝖑𝖔𝖘𝖊 ✞◥", data="close")]
     if len(reply_pop_up_alert) >= 4096:
         crackexy = "`Pasting Your Help Menu.`"
         await event.answer(crackexy, cache_time=0, alert=True)
@@ -240,25 +206,16 @@ async def on_plug_in_callback_query_handler(event):
         url = "https://del.dog/documents"
         r = requests.post(url, data=out_file.encode("UTF-8")).json()
         url = f"https://del.dog/{r['key']}"
+        
         await event.edit(
             f"Pasted {plugin_name} to {url}",
             link_preview=False,
-            buttons=[
-                [
-                    custom.Button.inline("◤✞ 𝕸𝖆𝖎𝖓 𝕸𝖊𝖓𝖚 ✞◥", data="backme"),
-                    custom.Button.inline("◤✞ 𝕮𝖑𝖔𝖘𝖊 ✞◥", data="close"),
-                ]
-            ],
+            buttons=fci
         )
     else:
         await event.edit(
             message=reply_pop_up_alert,
-            buttons=[
-                [
-                    custom.Button.inline("Go Back", data="backme"),
-                    custom.Button.inline("◤✞ 𝕮𝖑𝖔𝖘𝖊 ✞◥", data="close"),
-                ]
-            ],
+            buttons=fci
         )
 
 
