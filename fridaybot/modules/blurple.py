edfcd90cd12429aa7d36e74efc423ac73365e023
APXD-git <arkamtg.pramanik@gmail.com>
2020-12-04 16:43:33 +0000

    pylint: auto fixes

--- fridaybot/modules/blurple.py
+++ fridaybot/modules/blurple.py
@@ -1,27 +1,23 @@
-#Made By Sh1vam  Donot KANG
-# ME MADE MORE THAN ONE AND MORE COMPLEX ONE WAS YT COMMENT 
-#I REMOVED COLOUR CAUSE ALL NEED TO REMEMBER HEX COLOUR CODES # replaced by %23
+# Made By Sh1vam  Donot KANG
+# ME MADE MORE THAN ONE AND MORE COMPLEX ONE WAS YT COMMENT
+# I REMOVED COLOUR CAUSE ALL NEED TO REMEMBER HEX COLOUR CODES # replaced by %23
 import os
 
-
-import numpy as np
 import requests
-from PIL import Image
 from telegraph import upload_file
 from telethon.tl.types import MessageMediaPhoto
-import re
-from fridaybot.utils import admin_cmd
+
 from fridaybot import bot as borg
+from fridaybot.utils import admin_cmd
+
 sedpath = "./shivam/"
 if not os.path.isdir(sedpath):
     os.makedirs(sedpath)
 
-#keep CREDIT LINES ELSE GET LOST
-
+# keep CREDIT LINES ELSE GET LOST
 
 
 @bot.on(admin_cmd(pattern=r"blur"))
-
 async def lolmetrg(event):
     await event.delete()
     sed = await event.get_reply_message()
@@ -38,14 +34,13 @@ async def lolmetrg(event):
     r = requests.get(lolul)
     open("shivam.png", "wb").write(r.content)
     lolbruh = "shivam.png"
-    await borg.send_file(
-        event.chat_id, lolbruh, caption="blur", reply_to=sed
-    )
+    await borg.send_file(event.chat_id, lolbruh, caption="blur", reply_to=sed)
     for files in (lolbruh, img):
         if files and os.path.exists(files):
             os.remove(files)
-@bot.on(admin_cmd(pattern=r"blurple "))
 
+
+@bot.on(admin_cmd(pattern=r"blurple "))
 async def lolmetrg(event):
     await event.delete()
     sed = await event.get_reply_message()
@@ -62,9 +57,7 @@ async def lolmetrg(event):
     r = requests.get(lolul)
     open("shivam.png", "wb").write(r.content)
     lolbruh = "shivam.png"
-    await borg.send_file(
-        event.chat_id, lolbruh, caption="blurple ", reply_to=sed
-    )
+    await borg.send_file(event.chat_id, lolbruh, caption="blurple ", reply_to=sed)
     for files in (lolbruh, img):
         if files and os.path.exists(files):
             os.remove(files)
