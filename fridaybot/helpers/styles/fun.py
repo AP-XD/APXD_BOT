4db2a569c5420fc0d0b917ca7a68b512cc8322f6
APXD-git <63340491+APXD-git@users.noreply.github.com>
2021-01-25 23:45:17 +0530

    ADD DC

---
+++ fridaybot/helpers/styles/fun.py
@@ -0,0 +1,20 @@
+from fridaybot.function.funts import DC_STYLE as DC_FONT_STYLE
+from fridaybot.function.funts import DARKFONTS as DCFONTS
+from fridaybot.function.funts import SIMPLESTDC as SIMPLEDC
+
+
+
+
+
+
+
+
+
+#Kanger
+
+
+#tEri maa ki chut
+
+
+
+#useless
